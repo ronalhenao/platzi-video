a58789bdc8a85edf51dc6bdae2a77fc147d829a9
Ronal henao <ronalhenaodg@gmail.com>
2021-01-13 23:54:42 -0500

    remove favorite

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -25,12 +25,12 @@ const Home = ({ myList, trends, originals }) => {
                             <CarouselItem 
                                 key={ item.id }
                                 { ...item }
+                                isList
                             />
                         )}
                     </Carousel>
                 </Categories>
             )}
-
 
             <Categories title="Tendencias">
                 <Carousel>
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,18 @@
 const reducer = ( state, action ) => {
     switch ( action.type ) {
         case 'SET_FAVORITE':
+            // validación evitar que el mismo elemento se agregue a la lista favoritos
+            const exist = state.myList.find( item => item.id === action.payload.id )
+            if ( exist ) return { ...state }
+            
             return {
                 ...state,
                 myList: [...state.myList, action.payload]
+            }
+        case 'DELETE_FAVORITE':
+            return {
+                ...state,
+                myList: state.myList.filter( items => items.id !== action.payload )
             }
         default:
             return state;
